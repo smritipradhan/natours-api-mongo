b916f7098f4d756f66072a77f2a7089f1e24346e
smritipradhan <smritipradhan545@gmail.com>
2023-03-11 22:08:32 +0530

    Mongoose Install

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Natours API with MONGO
-Connecting Database with Express App
+
+## Connecting Database with Express App
+
+We connect our Application to the Database using a Driver. Mongoose is Object Data Modelling built on top of Mongo DB Driver providing developer with a way to model thier data. It is akin to an Object Relational Mapper (ORM) such as
+SQLAlchemy for traditional SQL databases. The problem that Mongoose aims to solve is allowing developers to enforce a specific schema at the application layer.
+For more information refer - https://www.mongodb.com/developer/languages/javascript/mongoose-versus-nodejs-driver/
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE;
 
+dotenv.config({ path: './config.env' });
+console.log(DB);
+mongoose
+  .connect(
+    'mongodb+srv://smritipradhan:smriti@cluster0.zeoz99e.mongodb.net/natours-test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(con => {
+    console.log('Hey');
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
